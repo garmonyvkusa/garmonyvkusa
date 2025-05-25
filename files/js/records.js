3a402973e99f41c5182d7fbc1f0558a913ca4184
Daniil Buchko <14076544@cuni.cz>
2025-05-25 12:56:00 +0200

    update

--- files/js/records.js
+++ files/js/records.js
@@ -9,19 +9,23 @@ function onclickrec(id) {
 		var h = window.innerHeight;
     if (document.getElementById("iframe")==null) {}
     else {
-    document.getElementById('iframe').setAttribute("style", "height: " + (document.getElementById("iframe").offsetWidth * 0.5625 - 9).toString() + "px;");
+    document.getElementById('iframe').setAttribute("style",
+    "height: " + (document.getElementById("iframe").offsetWidth * 0.5625 - 9).toString() + "px;");
     }
 		if (h<500) {
-				document.getElementById("computer").setAttribute("hidden", "");
+				document.getElementById("computer").setAttribute("hidden",
+    "");
 				document.getElementById("mobile").removeAttribute("hidden");
 		}
 		else if (w<1220) {
-			document.getElementById("computer").setAttribute("hidden", "");
+			document.getElementById("computer").setAttribute("hidden",
+    "");
 			document.getElementById("mobile").removeAttribute("hidden");
 		}
 		else {
 			document.getElementById("computer").removeAttribute("hidden");
-			document.getElementById("mobile").setAttribute("hidden", "");
+			document.getElementById("mobile").setAttribute("hidden",
+    "");
 		}
 
 }
